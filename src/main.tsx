94c86a32f849e24b782451d91b61a30edf9b5889
kangin <kangin@i-ops.co.kr>
2026-09-18 08:53:02 +0900

    tanstackquery devtools 버튼 왼쪽 하단으로 이동

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left"/>
     </QueryClientProvider>
   </StrictMode>,
 );
